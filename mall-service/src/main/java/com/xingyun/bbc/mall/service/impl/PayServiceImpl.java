1798d1655865f0db28d8365d7538973b0e628dd1
蒋辉 <jiang.hui@xyb2b.com>
2019-09-23 15:37:19 +0800

    支付密码解码

--- mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/PayServiceImpl.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/PayServiceImpl.java
@@ -272,7 +272,7 @@ public class PayServiceImpl implements PayService {
 				logger.info("校验余额支付失败：未设置支付密码！订单号：" + forderId + "，用户名：" + fuid + "，余额类型：" + balanceType);
 				return Result.failure(MallExceptionCode.PAY_PWD_IS_NOT_SET);
 			}
-			if (!payPwdMd5.equals(inputPayPwd)) {
+			if (!EncryptUtils.aesDecrypt(payPwdMd5).equals(inputPayPwd)) {
 				logger.info("校验余额支付失败：支付密码错误！订单号：" + forderId + "，用户名：" + fuid + "，余额类型：" + balanceType);
 				return Result.failure(MallExceptionCode.WITHDRAW_PSD_WRONG);
 			}
