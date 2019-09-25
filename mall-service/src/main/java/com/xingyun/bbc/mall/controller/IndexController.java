bbace9de9294aba1c7d14feb6b692d4de81b2acd
liulele <liu.lele@xyb2b.com>
2019-09-25 14:58:43 +0800

    修改首页登录校验

--- mall-service/src/main/java/com/xingyun/bbc/mall/controller/IndexController.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/controller/IndexController.java
@@ -51,13 +51,13 @@ public class IndexController {
 
     @ApiImplicitParams({@ApiImplicitParam(paramType = "query", dataType = "Integer", name = "fposition", value = "导航栏位置(0Banner配置 1ICON配置 2专题位配置", required = false)})
     @ApiOperation(value = "查询首页配置", httpMethod = "POST")
-    @PostMapping("/getConfig")
+    @PostMapping("/via/getConfig")
     public Result<List<PageConfigVo>> getConfig(@RequestParam(value = "fposition", required = true) Integer fposition) {
         return indexService.getConfig(fposition);
     }
 
     @ApiOperation(value = "查询商品一级类目列表")
-    @GetMapping("/queryGoodsCategoryList")
+    @GetMapping("/via/queryGoodsCategoryList")
     public Result<List<IndexGoodsCategoryVo>> queryGoodsCategoryList(HttpServletRequest request) {
         log.info("查询商品一级类目列表: " + Thread.currentThread().getStackTrace()[1].getMethodName());
         Result<List<GoodsCategoryVo>> result = categoryService.queryGoodsCategoryList();
@@ -66,9 +66,18 @@ public class IndexController {
     }
 
 
-    @ApiOperation(value = "查询一级类目下所有sku列表")
-    @PostMapping("/queryGoodsByCategoryId1")
+    @ApiOperation(value = "查询一级类目下所有sku列表---不校验登录")
+    @PostMapping("/via/queryGoodsByCategoryId1")
     public Result<PageVo<IndexSkuGoodsVo>> queryGoodsByCategoryId1(CategoryDto categoryDto, HttpServletRequest request) {
+        //categoryDto.setFuid(Long.parseLong(request.getHeader("xyid")));
+        //categoryDto.setFuid(Long.valueOf(10157));
+        return Result.success(indexService.queryGoodsByCategoryId1(categoryDto));
+    }
+
+
+    @ApiOperation(value = "查询一级类目下所有sku列表---校验登录")
+    @PostMapping("/queryGoodsByCategoryId1OnLogin")
+    public Result<PageVo<IndexSkuGoodsVo>> queryGoodsByCategoryId1OnLogin(CategoryDto categoryDto, HttpServletRequest request) {
         categoryDto.setFuid(Long.parseLong(request.getHeader("xyid")));
         //categoryDto.setFuid(Long.valueOf(10157));
         return Result.success(indexService.queryGoodsByCategoryId1(categoryDto));
