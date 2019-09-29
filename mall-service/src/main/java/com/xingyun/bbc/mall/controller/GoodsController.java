730969ba0a588073a62a5065eaec41e40cfb6f5f
deluxo <li.zhanyuan@xyb2b.com>
2019-09-29 11:14:54 +0800

    搜索接口路径变更，免除token验证

--- mall-service/src/main/java/com/xingyun/bbc/mall/controller/GoodsController.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/controller/GoodsController.java
@@ -8,14 +8,18 @@ import com.xingyun.bbc.mall.model.vo.PageVo;
 import com.xingyun.bbc.mall.model.vo.SearchFilterVo;
 import com.xingyun.bbc.mall.model.vo.SearchItemVo;
 import com.xingyun.bbc.mall.service.GoodsService;
+import com.xingyun.bbc.search.api.MallSkuSearchApi;
+import com.xingyun.bbc.search.criteria.EsCriteria;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.elasticsearch.common.lucene.search.function.CombineFunction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 
 @Api("商品")
 @RestController
@@ -24,34 +28,35 @@ public class GoodsController {
 
     @Autowired
     GoodsService goodsService;
+    @Autowired
+    MallSkuSearchApi mallSkuSearchApi;
 
     @ApiOperation("查询商品列表")
-    @PostMapping("/skuSearch")
+    @PostMapping("/via/skuSearch")
     public Result<PageVo<SearchItemVo>> skuSearch(@RequestBody SearchItemDto dto, HttpServletRequest request) {
-        if(request.getHeader("xyid") != null){
-            dto.setFuid(Integer.parseInt(request.getHeader("xyid")));
+        if(request.getHeader("xyId") != null){
+            dto.setFuid(Integer.parseInt(request.getHeader("xyId")));
         }
         return goodsService.searchSkuList(dto);
     }
 
     @ApiOperation("查询筛选信息")
-    @PostMapping("/skuSearchFilter")
+    @PostMapping("/via/skuSearchFilter")
     public Result<SearchFilterVo> skuSearchFilter(@RequestBody SearchItemDto dto, HttpServletRequest request){
         return goodsService.searchSkuFilter(dto);
     }
 
     @ApiOperation(value = "查询品牌专属页")
-    @PostMapping("/skuSearchBrandPage")
+    @PostMapping("/via/skuSearchBrandPage")
     public Result<BrandPageVo> skuSearchBrandPage(@ApiParam("品牌id") @RequestParam Integer fbrandId, HttpServletRequest request){
         return goodsService.searchSkuBrandPage(fbrandId);
     }
 
     @ApiOperation(value = "查询热门搜索")
-    @PostMapping("/queryHotSearch")
+    @PostMapping("/via/queryHotSearch")
     public Result<List<String>> queryHotSearch(){
         return goodsService.queryHotSearch();
     }
-
 
 
 }
