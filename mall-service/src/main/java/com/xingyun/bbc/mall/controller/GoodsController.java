dece121635fa9afd7e3e79cba5b60468a887d0a4
deluxo <li.zhanyuan@xyb2b.com>
2019-10-19 19:00:47 +0800

    搜索接口更新

--- mall-service/src/main/java/com/xingyun/bbc/mall/controller/GoodsController.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/controller/GoodsController.java
@@ -1,17 +1,15 @@
 package com.xingyun.bbc.mall.controller;
 
 
-import com.xingyun.bbc.common.jwt.XyUserJwtManager;
 import com.xingyun.bbc.core.utils.Result;
+import com.xingyun.bbc.mall.base.utils.JwtParser;
 import com.xingyun.bbc.mall.model.dto.SearchItemDto;
 import com.xingyun.bbc.mall.model.vo.*;
 import com.xingyun.bbc.mall.service.GoodsService;
 import com.xingyun.bbc.mall.service.UserService;
-import io.jsonwebtoken.Claims;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,26 +21,30 @@ import java.util.List;
 @RequestMapping("/goods")
 public class GoodsController {
 
-    private static final String ACCESS_TOKEN = "accessToken";
 
     @Autowired
     GoodsService goodsService;
     @Autowired
     UserService userService;
     @Autowired
-    private XyUserJwtManager userJwtManager;
+    JwtParser jwtParser;
+
 
     @ApiOperation("查询商品列表")
     @PostMapping("/via/skuSearch")
     public Result<SearchItemListVo<SearchItemVo>> skuSearch(@RequestBody SearchItemDto dto, HttpServletRequest request) {
-        this.getTokenInfo(dto, request);
+        TokenInfoVo infoVo = jwtParser.getTokenInfo(request);
+        dto.setIsLogin(infoVo.getIsLogin());
+        dto.setFuid(infoVo.getFuid());
         return goodsService.searchSkuList(dto);
     }
 
     @ApiOperation("查询筛选信息")
     @PostMapping("/via/skuSearchFilter")
     public Result<SearchFilterVo> skuSearchFilter(@RequestBody SearchItemDto dto, HttpServletRequest request){
-        this.getTokenInfo(dto, request);
+        TokenInfoVo infoVo = jwtParser.getTokenInfo(request);
+        dto.setIsLogin(infoVo.getIsLogin());
+        dto.setFuid(infoVo.getFuid());
         return goodsService.searchSkuFilter(dto);
     }
 
@@ -58,24 +60,6 @@ public class GoodsController {
         return goodsService.queryHotSearch();
     }
 
-//    @ApiOperation("生成token")
-//    @PostMapping("/generateToken")
-//    public Result<String> generateToken(@RequestParam String fuid, @RequestParam String fmobile){
-//        return userService.generateToken(fuid, fmobile);
-//    }
 
-    private void getTokenInfo(SearchItemDto dto, HttpServletRequest request){
-        if(StringUtils.isNotEmpty(request.getHeader(ACCESS_TOKEN))){
-            String token = request.getHeader(ACCESS_TOKEN);
-            Claims claims = userJwtManager.parseJwt(token);
-            if (claims == null) {
-                dto.setIsLogin(false);
-                return;
-            }
-            String id = claims.getId();
-            dto.setIsLogin(true);
-            dto.setFuid(Integer.parseInt(id));
-        }
-    }
 
 }
