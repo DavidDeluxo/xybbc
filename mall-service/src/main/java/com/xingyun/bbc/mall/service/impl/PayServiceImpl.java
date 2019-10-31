964e48f422f41965cb900efaf8c22353dbf20740
蒋辉 <jiang.hui@xyb2b.com>
2019-10-31 15:02:52 +0800

    修改log打印

--- mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/PayServiceImpl.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/PayServiceImpl.java
@@ -290,7 +290,7 @@ public class PayServiceImpl implements PayService {
 			
 			if("200".equals(thirdPay.getData().getCode().toString())||"1030".equals(thirdPay.getData().getCode().toString()))// 200:处理成功,1030:订单已支付
 			{
-				logger.info("------------------第三方支付回调请求订单中心返回："+thirdPayUtil.thirdPayNotifySuccess(response));
+				logger.info("------------------第三方支付回调请求订单中心返回成功"+thirdPayUtil.thirdPayNotifySuccess(response));
 			}
 			
 			return thirdPay;
