a6ba5d3e3180182862a82b387fdbe4fe1366d2d5
何开金 <he.kaijin@xyb2b.com>
2019-11-01 11:52:12 +0800

    提现 密码md5更换

--- mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/WalletServiceImpl.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/WalletServiceImpl.java
@@ -16,10 +16,7 @@ import com.xingyun.bbc.core.user.po.*;
 import com.xingyun.bbc.core.utils.Result;
 import com.xingyun.bbc.core.utils.StringUtil;
 import com.xingyun.bbc.mall.base.enums.MallResultStatus;
-import com.xingyun.bbc.mall.base.utils.EncryptUtils;
-import com.xingyun.bbc.mall.base.utils.MD5Util;
-import com.xingyun.bbc.mall.base.utils.PriceUtil;
-import com.xingyun.bbc.mall.base.utils.RandomUtils;
+import com.xingyun.bbc.mall.base.utils.*;
 import com.xingyun.bbc.mall.common.constans.MallRedisConstant;
 import com.xingyun.bbc.mall.common.ensure.Ensure;
 import com.xingyun.bbc.mall.common.exception.MallExceptionCode;
@@ -311,7 +308,7 @@ public class WalletServiceImpl implements WalletService {
 
         if (StringUtil.isBlank(passWord)) throw new BizException(MallResultStatus.WITHDRAW_PASSWORD_ERROR);
 
-        passWord = MD5Util.MD5EncodeUtf8(passWord);
+        passWord = Md5Utils.toMd5(passWord);
 
         if (!passWord.equals(user.getFwithdrawPasswd()))
             throw new BizException(MallResultStatus.WITHDRAW_PASSWORD_ERROR);
