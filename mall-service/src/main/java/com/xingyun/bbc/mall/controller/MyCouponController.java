fb3a465cddca0243325bcd46db3446ed0f20e691
e <yang.li@xyb2b.com>
2019-11-13 13:53:13 +0800

    我的 优惠券列表

---
+++ mall-service/src/main/java/com/xingyun/bbc/mall/controller/MyCouponController.java
@@ -0,0 +1,32 @@
+package com.xingyun.bbc.mall.controller;
+
+import com.xingyun.bbc.core.utils.Result;
+import com.xingyun.bbc.mall.model.dto.MyCouponDto;
+import com.xingyun.bbc.mall.model.vo.MyCouponVo;
+import com.xingyun.bbc.mall.service.MyCouponService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Api("我的优惠券")
+@RestController
+@RequestMapping("/myCoupon")
+public class MyCouponController {
+
+    @Autowired
+    private MyCouponService myCouponService;
+
+    @ApiOperation(value = "获取我的优惠券列表", httpMethod = "GET")
+    @GetMapping("/getMyCouponLis")
+    public Result<MyCouponVo> getMyCouponLis(@ModelAttribute MyCouponDto myCouponDto, HttpServletRequest request) {
+        Long xyid = Long.parseLong(request.getHeader("xyid"));
+        myCouponDto.setFuid(xyid);
+        return myCouponService.getMyCouponVo(myCouponDto);
+    }
+}
