762bb0216c5403638c079dc0d0f45ce60e31297c
yanghuaiGit <2774584057@qq.com>
2019-11-22 11:52:43 +0800

    我的账户

--- mallpc-service/src/main/java/com/xingyun/bbc/mallpc/controller/PageConfigController.java
+++ mallpc-service/src/main/java/com/xingyun/bbc/mallpc/controller/PageConfigController.java
@@ -2,15 +2,20 @@ package com.xingyun.bbc.mallpc.controller;
 
 import com.xingyun.bbc.core.utils.Result;
 import com.xingyun.bbc.mallpc.model.vo.pageconfig.PageConfigPcVo;
+import com.xingyun.bbc.mallpc.service.PageConfigPcService;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
 @RequestMapping("pageconfig")
 public class PageConfigController {
+    @Resource
+    private PageConfigPcService pageConfigPcService;
     /**
      * 导航静态文件缓存 前端获取数据接口
      *
