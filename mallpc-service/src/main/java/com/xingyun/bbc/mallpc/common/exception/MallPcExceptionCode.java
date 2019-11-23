d294afb31a7202c06ce34eb642745313c2f97bf1
liulele <liu.lele@xyb2b.com>
2019-11-23 16:05:00 +0800

    新增异常编码

--- mallpc-service/src/main/java/com/xingyun/bbc/mallpc/common/exception/MallPcExceptionCode.java
+++ mallpc-service/src/main/java/com/xingyun/bbc/mallpc/common/exception/MallPcExceptionCode.java
@@ -137,4 +137,5 @@ public class MallPcExceptionCode implements IResultStatus {
     public static final MallPcExceptionCode CODE_NOT_COUPON = new MallPcExceptionCode("6006", "券码无匹配优惠券");
     public static final MallPcExceptionCode USER_NOT_COUPON = new MallPcExceptionCode("6007", "用户无匹配优惠券");
     public static final MallPcExceptionCode USER_NOT_RIGHT_COUPON = new MallPcExceptionCode("6008", "用户暂无权限兑换该类优惠券");
+    public static final MallPcExceptionCode COUPON_IS_NOT_TIME = new MallPcExceptionCode("6009", "优惠券不在领取时间内");
 }
