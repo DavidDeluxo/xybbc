d4fcc0479fd6b324be18cded26e71c44f963a784
何开金 <he.kaijin@xyb2b.com>
2019-11-29 14:17:23 +0800

    添加购物车购买数量更新 接口...

--- mall-service/src/main/java/com/xingyun/bbc/mall/controller/CartController.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/controller/CartController.java
@@ -1,12 +1,16 @@
 package com.xingyun.bbc.mall.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.xingyun.bbc.core.order.api.ShopcarApi;
+import com.xingyun.bbc.core.order.po.Shopcar;
+import com.xingyun.bbc.core.query.Criteria;
 import com.xingyun.bbc.core.utils.Result;
 import com.xingyun.bbc.mall.base.utils.UserUtil;
 import com.xingyun.bbc.order.api.CartApi;
 import com.xingyun.bbc.order.model.dto.cart.*;
 import com.xingyun.bbc.order.model.vo.PageVo;
 import com.xingyun.bbc.order.model.vo.cart.CartsVo;
+import io.seata.common.util.CollectionUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +21,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author hekaijin
@@ -32,6 +38,8 @@ public class CartController {
 
     @Autowired
     private CartApi cartApi;
+    @Autowired
+    private ShopcarApi shopcarApi;
 
     @ApiOperation("添加购物车")
     @PostMapping("/addCart")
@@ -47,6 +55,16 @@ public class CartController {
     public Result<PageVo<CartsVo>> queryCart(@RequestBody @Valid QueryCartDto queryCartDto, HttpServletRequest request) {
         Long fuid = UserUtil.uid(request);
         log.info("|接口:|查询购物车|请求参数:{}|", JSON.toJSONString(queryCartDto));
+        try {
+            Result<List<Shopcar>> idsRes = shopcarApi.queryByCriteria(Criteria.of(Shopcar.class).andEqualTo(Shopcar::getFuid, fuid).fields(Shopcar::getFshopcarId));
+            if (idsRes.isSuccess()&& CollectionUtils.isNotEmpty(idsRes.getData())){
+                List<Long> ids = idsRes.getData().stream().map(Shopcar::getFshopcarId).collect(Collectors.toList());
+                cartApi.refresh(new CartRefreshDto().setShopCarIds(ids).setUserId(fuid));
+            }
+        }catch (Exception e){
+            log.warn("购物车刷新数量无效...");
+        }
+
         return cartApi.queryCart(queryCartDto.setFuid(fuid));
     }
 
@@ -70,7 +88,6 @@ public class CartController {
         Long fuid = UserUtil.uid(request);
         return cartApi.modifyNum(cartNumDto.setFuid(fuid));
     }
-
 
     @ApiOperation("刷新商品")
     @PostMapping("refresh")
