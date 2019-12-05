5728f100fc2b64239e38b3ce6e7a398eab5ad06d
e <yang.li@xyb2b.com>
2019-12-05 15:32:56 +0800

    我的优惠券返回条数

--- mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/MyCouponServiceImpl.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/MyCouponServiceImpl.java
@@ -58,50 +58,50 @@ public class MyCouponServiceImpl implements MyCouponService {
         if (!countResult.isSuccess()){
             throw new BizException(ResultStatus.REMOTE_SERVICE_ERROR);
         }
-        if (countResult.getData().intValue() == 0) {
-            return Result.success();
-        }
-        Result<List<CouponReceive>> listResult = couponReceiveApi.selectMyCouponList(myCoupinReceiveDto);
-        if (!listResult.isSuccess()) {
-            throw new BizException(ResultStatus.REMOTE_SERVICE_ERROR);
-        }
-
-        Integer count = countResult.getData().intValue();
-        List<CouponReceive> couponReceives = listResult.getData();
-        PageVo<CouponVo> couponPageVo = pageUtils.convert(count, couponReceives, CouponVo.class, myCouponDto);
-        List<Long> couponIds = couponReceives.stream().map(CouponReceive::getFcouponId).distinct().collect(Collectors.toList());
-        Result<List<Coupon>> couponResult = couponApi.queryByCriteria(Criteria.of(Coupon.class)
-                .andIn(Coupon::getFcouponId, couponIds)
-                .fields(Coupon::getFcouponId, Coupon::getFcouponName, Coupon::getFcouponType,
-                        Coupon::getFthresholdAmount, Coupon::getFdeductionValue,
-                        Coupon::getFvalidityType, Coupon::getFvalidityDays, Coupon::getFreleaseType));
-        List<Coupon> couponList = ResultUtils.getListNotEmpty(couponResult, MallExceptionCode.COUPON_IS_NOT_EXIST);
-        Map<Long, List<Coupon>> couponMap = couponList.stream().collect(Collectors.groupingBy(Coupon::getFcouponId));
-
-        //查询优惠券信息
-        for (CouponVo couponVo : couponPageVo.getList()) {
-            Coupon coupon = couponMap.get(couponVo.getFcouponId()).get(0);
-            if (Objects.nonNull(coupon)) {
-                couponVo.setFcouponName(coupon.getFcouponName());
-                couponVo.setFcouponType(coupon.getFcouponType());
-                couponVo.setFthresholdAmount(PriceUtil.toYuan(coupon.getFthresholdAmount()));
-
-                //优惠券类型，1满减券、2折扣券
-                if (coupon.getFcouponType().equals(CouponTypeEnum.FULL_REDUCTION.getCode())) {
-                    couponVo.setFdeductionValue(PriceUtil.toYuan(coupon.getFdeductionValue()));
-                } else {
-                    couponVo.setFdeductionValue(new BigDecimal(coupon.getFdeductionValue()).divide(new BigDecimal("10"), 1, BigDecimal.ROUND_HALF_UP));
-                }
-                couponVo.setFvalidityType(coupon.getFvalidityType());
-                couponVo.setFvalidityDays(coupon.getFvalidityDays());
-                couponVo.setFreleaseType(coupon.getFreleaseType());
+        MyCouponVo myCouponVo = new MyCouponVo();
+        int count = countResult.getData().intValue();
+        if (count != 0) {
+            Result<List<CouponReceive>> listResult = couponReceiveApi.selectMyCouponList(myCoupinReceiveDto);
+            if (!listResult.isSuccess()) {
+                throw new BizException(ResultStatus.REMOTE_SERVICE_ERROR);
             }
+
+            List<CouponReceive> couponReceives = listResult.getData();
+            PageVo<CouponVo> couponPageVo = pageUtils.convert(count, couponReceives, CouponVo.class, myCouponDto);
+            List<Long> couponIds = couponReceives.stream().map(CouponReceive::getFcouponId).distinct().collect(Collectors.toList());
+            Result<List<Coupon>> couponResult = couponApi.queryByCriteria(Criteria.of(Coupon.class)
+                    .andIn(Coupon::getFcouponId, couponIds)
+                    .fields(Coupon::getFcouponId, Coupon::getFcouponName, Coupon::getFcouponType,
+                            Coupon::getFthresholdAmount, Coupon::getFdeductionValue,
+                            Coupon::getFvalidityType, Coupon::getFvalidityDays, Coupon::getFreleaseType));
+            List<Coupon> couponList = ResultUtils.getListNotEmpty(couponResult, MallExceptionCode.COUPON_IS_NOT_EXIST);
+            Map<Long, List<Coupon>> couponMap = couponList.stream().collect(Collectors.groupingBy(Coupon::getFcouponId));
+
+            //查询优惠券信息
+            for (CouponVo couponVo : couponPageVo.getList()) {
+                Coupon coupon = couponMap.get(couponVo.getFcouponId()).get(0);
+                if (Objects.nonNull(coupon)) {
+                    couponVo.setFcouponName(coupon.getFcouponName());
+                    couponVo.setFcouponType(coupon.getFcouponType());
+                    couponVo.setFthresholdAmount(PriceUtil.toYuan(coupon.getFthresholdAmount()));
+
+                    //优惠券类型，1满减券、2折扣券
+                    if (coupon.getFcouponType().equals(CouponTypeEnum.FULL_REDUCTION.getCode())) {
+                        couponVo.setFdeductionValue(PriceUtil.toYuan(coupon.getFdeductionValue()));
+                    } else {
+                        couponVo.setFdeductionValue(new BigDecimal(coupon.getFdeductionValue()).divide(new BigDecimal("10"), 1, BigDecimal.ROUND_HALF_UP));
+                    }
+                    couponVo.setFvalidityType(coupon.getFvalidityType());
+                    couponVo.setFvalidityDays(coupon.getFvalidityDays());
+                    couponVo.setFreleaseType(coupon.getFreleaseType());
+                }
+            }
+            myCouponVo.setCouponVo(couponPageVo);
         }
+
         //查询各种优惠券数量
         Integer fuserCouponStatus = myCouponDto.getFuserCouponStatus();
 
-        MyCouponVo myCouponVo = new MyCouponVo();
-        myCouponVo.setCouponVo(couponPageVo);
         myCouponVo.setUnUsedNum(fuserCouponStatus.equals(CouponReceiveStatusEnum.NOT_USED.getCode()) ? count
                 : this.getCouponByStatus(myCouponDto, CouponReceiveStatusEnum.NOT_USED.getCode()));
         myCouponVo.setUsedNum(fuserCouponStatus.equals(CouponReceiveStatusEnum.USED.getCode()) ? count
