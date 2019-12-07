f33fcbfcd8d5c60918a784418a6447d25cee4496
liulele <liu.lele@xyb2b.com>
2019-12-07 17:39:33 +0800

    优化代码

--- mall-service/src/main/java/com/xingyun/bbc/mall/controller/IndexController.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/controller/IndexController.java
@@ -86,7 +86,7 @@ public class IndexController {
     }
 
 
-    @ApiOperation(value = "查询一级类目下所有sku列表---不校验登录")
+    @ApiOperation(value = "查询一级类目下所有sku列表")
     @PostMapping("/via/queryGoodsByCategoryId1")
     public Result<SearchItemListVo<SearchItemVo>> queryGoodsByCategoryId1(@RequestBody SearchItemDto dto, HttpServletRequest request) {
         TokenInfoVo infoVo = jwtParser.getTokenInfo(request);
