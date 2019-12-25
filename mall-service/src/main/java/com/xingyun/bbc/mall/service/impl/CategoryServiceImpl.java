d7a6f4841ccfd24b40746309f8d6cef4fa3d685c
lizhanyuan <li.zhanyuan@xyb2b.com>
2019-12-25 18:50:44 +0800

    品牌列表类目查询更新

--- mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/CategoryServiceImpl.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/CategoryServiceImpl.java
@@ -234,13 +234,12 @@ public class CategoryServiceImpl implements CategoryService {
                 .andEqualTo(GoodsCategory::getFisDelete, 0)
                 //类目展示
                 .andEqualTo(GoodsCategory::getFisDisplay, 1)
-                //修改时间倒序
-                .sortDesc(GoodsCategory::getFmodifyTime));
+                //修改排序字段排序
+                .sort(GoodsCategory::getFcategorySort));
         Ensure.that(categoryListResultAll.isSuccess()).isTrue(MallExceptionCode.PARAM_ERROR);
         if(CollectionUtils.isEmpty(categoryListResultAll.getData())){
             return Result.success(categoryVoList);
         }
-
         //过滤没有商品的一级类目
         List<GoodsCategory> filteredCategories = new LinkedList<>();
         for (GoodsCategory category : categoryListResultAll.getData()){
