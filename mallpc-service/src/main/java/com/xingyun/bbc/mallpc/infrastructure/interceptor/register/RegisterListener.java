c5055b4b69a03746cecf4883927ba21632553c26
penglu <lu.peng@xyb2b.com>
2019-12-26 11:40:04 +0800

    move package

--- mallpc-service/src/main/java/com/xingyun/bbc/mallpc/infrastructure/interceptor/register/RegisterListener.java
+++ mallpc-service/src/main/java/com/xingyun/bbc/mallpc/infrastructure/interceptor/register/RegisterListener.java
@@ -1,11 +1,11 @@
-package com.xingyun.bbc.mallpc.infrastructure.event.register;
+package com.xingyun.bbc.mallpc.infrastructure.interceptor.register;
 
-import com.xingyun.bbc.core.operate.enums.PushTypeEnum;
 import com.xingyun.bbc.mallpc.common.exception.MallPcExceptionCode;
 import com.xingyun.bbc.message.business.MessagePushChannel;
 import com.xingyun.bbc.message.business.WaitSendInfo;
 import com.xingyun.bbc.message.model.dto.MsgPushDto;
 import com.xingyun.bbc.message.model.dto.MsgTemplateVariableDto;
+import com.xingyun.bbc.message.model.enums.PushTypeEnum;
 import io.jsonwebtoken.lang.Assert;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.ApplicationListener;
@@ -17,6 +17,7 @@ import javax.annotation.Resource;
 
 /**
  * 注册监听
+ *
  * @author: xuxianbei
  * Date: 2019/12/24
  * Time: 14:11
@@ -35,7 +36,7 @@ public class RegisterListener implements ApplicationListener<RegisterEvent> {
         MsgPushDto msgPushDto = new MsgPushDto();
         MsgTemplateVariableDto msgTemplateVariableDto = new MsgTemplateVariableDto();
         Assert.isTrue(event.getSource() instanceof WaitSendInfo, MallPcExceptionCode.SYSTEM_ERROR.getMsg());
-        WaitSendInfo waitSendInfo =  (WaitSendInfo) event.getSource();
+        WaitSendInfo waitSendInfo = (WaitSendInfo) event.getSource();
         msgTemplateVariableDto.setFmobile(waitSendInfo.getOldKey());
         msgPushDto.setMsgTemplateVariable(msgTemplateVariableDto);
         msgPushDto.setPushType(PushTypeEnum.SYSTEM_NOTIFY.getKey());
