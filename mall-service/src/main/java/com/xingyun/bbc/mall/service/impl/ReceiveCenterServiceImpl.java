2f9658ad292109fdc6ed2fd4e226b1d0729b0b89
liulele <liu.lele@xyb2b.com>
2020-01-10 15:04:24 +0800

    折扣券除以100展示，修改极端值bug

--- mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/ReceiveCenterServiceImpl.java
+++ mall-service/src/main/java/com/xingyun/bbc/mall/service/impl/ReceiveCenterServiceImpl.java
@@ -263,7 +263,7 @@ public class ReceiveCenterServiceImpl implements ReceiveCenterService {
                     if (couponQueryVo.getFcouponType().equals(CouponTypeEnum.FULL_REDUCTION.getCode())) {
                         receiveCenterCouponVo.setFdeductionValue(PriceUtil.toYuan(couponQueryVo.getFdeductionValue()));
                     } else {
-                        receiveCenterCouponVo.setFdeductionValue(new BigDecimal(couponQueryVo.getFdeductionValue()).divide(new BigDecimal("100"), 2));
+                        receiveCenterCouponVo.setFdeductionValue(new BigDecimal(couponQueryVo.getFdeductionValue()).divide(new BigDecimal("100"), 2,BigDecimal.ROUND_DOWN));
                     }
                     receiveCenterCouponVoList.add(receiveCenterCouponVo);
                 }
