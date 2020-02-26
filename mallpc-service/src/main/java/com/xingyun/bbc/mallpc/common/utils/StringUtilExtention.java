d3b7d063b4303bcd84cc2eacb054ec7666216a4f
卢彭 <lu.peng@xyb2b.com>
2020-02-26 13:34:06 +0800

    fix-手机号码正则更新

--- mallpc-service/src/main/java/com/xingyun/bbc/mallpc/common/utils/StringUtilExtention.java
+++ mallpc-service/src/main/java/com/xingyun/bbc/mallpc/common/utils/StringUtilExtention.java
@@ -76,7 +76,7 @@ public class StringUtilExtention extends StringUtil {
         if(Strings.isNullOrEmpty(mobile)){
             return false;
         }
-        String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
+        String regex = "^1(3|4|5|6|7|8|9)\\d{9}$";
         if(mobile.length() != 11){
             return false;
         }else{
